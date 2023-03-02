2e154364463e75b3fa1f3141b3e3ff3f09e1c335
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-03-02 10:39:22 -0600

    Created networked player script

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,17 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    List<Unit> myUnits = new List<Unit>();
+
+    #region Server
+
+    #endregion
+
+    #region Client
+
+    #endregion
+}
