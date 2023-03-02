5c7ca29e801cceb65ab54e73ad653fc8767f5549
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-03-02 10:58:01 -0600

    Created events to handle units spawning and despawning so RTSPlayer can handle server/client code for these events

--- Assets/Scripts/Networking/RTSPlayer.cs
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -9,9 +9,59 @@ public class RTSPlayer : NetworkBehaviour
 
     #region Server
 
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+    }
+
+    void ServerHandleUnitSpawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Add(unit);
+    }
+
+    void ServerHandleUnitDespawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Remove(unit);
+    }
+
     #endregion
 
     #region Client
+
+    public override void OnStartAuthority()
+    {
+        if (NetworkServer.active) { return; }
+
+        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+    }
+
+    public override void OnStopClient()
+    {
+        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+    }
+
+    void AuthorityHandleUnitSpawned(Unit unit)
+    {
+        myUnits.Add(unit);
+    }
+
+    void AuthorityHandleUnitDespawned(Unit unit)
+    {
+        myUnits.Remove(unit);
+    }
 
     #endregion
 }
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,42 @@ public class Unit : NetworkBehaviour
 {
     [SerializeField] Targeter targeter = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
+
+    #region Client
+
     public override void OnStartClient()
     {
         gameObject.SetActive(true);
     }
+
+    public override void OnStartAuthority()
+    {
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopAuthority()
+    {
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
 }
