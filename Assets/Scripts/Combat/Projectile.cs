f8606e1049708900e1d1bcc14482afe627d0b648
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-03-06 07:11:41 -0600

    - Created projectiles for both ranged and melee - Fixed healthbar bug

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -5,12 +5,45 @@ using UnityEngine;
 
 public class Projectile : NetworkBehaviour
 {
+    [SerializeField] Rigidbody rb = null;
     [SerializeField] int damage = 10;
+    [SerializeField] bool isRanged = false;
+    [SerializeField] float launchForce = 10f;
+    [SerializeField] float destroyAfterSeconds = 5f;
 
+    private void Start()
+    {
+        if (!isRanged)
+        {
+            launchForce = 0f;
+        }
+
+        rb.velocity = transform.forward * launchForce;
+    }
+
+    public override void OnStartServer()
+    {
+        Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [ServerCallback]
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.TryGetComponent(out Health health)) { return; }
+        if (other.TryGetComponent(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) { return; }
+        }
 
-        health.DealDamage(damage);
+        if (other.TryGetComponent(out Health health))
+        {
+            health.DealDamage(damage);
+            DestroySelf();
+        }
+    }
+
+    [Server]
+    void DestroySelf()
+    {
+        NetworkServer.Destroy(gameObject);
     }
 }
